b51b228855f6a52704046b2d60283808e6150530
tanulo <mate.edina.1230@dszcbaross.edu.hu>
2026-09-22 10:04:35 +0200

    a 15 feladat

---
+++ SzamtaniKozep/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzamtaniKozep
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("add meg az első számot: ");
+            int szam1 = int.Parse(Console.ReadLine());
+
+            Console.Write("add meg a második számot: ");
+            int szam2 = int.Parse(Console.ReadLine());
+
+            double szamtaniKozep = (double)(szam1 + szam2) / 2;
+            Console.WriteLine($"A számok számtani közepe: {szamtaniKozep}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
